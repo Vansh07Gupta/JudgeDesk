905b96c114a4a948a69798e83c0e83daf556d0ce
AadiJain558 <aadijain558@gmail.com>
2025-04-16 21:46:42 +0530

    Develop custom hooks

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -1,40 +1,39 @@
-import SplitterComponent from "@/components/SplitterComponent"
-import ConnectionStatusPage from "@/components/connection/ConnectionStatusPage"
-import Sidebar from "@/components/sidebar/Sidebar"
-import VideoCall from "@/components/sidebar/sidebar-views/VideoCall"
-import WorkSpace from "@/components/workspace"
-import { useAppContext } from "@/context/AppContext"
-import { useSocket } from "@/context/SocketContext"
-import useFullScreen from "@/hooks/useFullScreen"
-import useUserActivity from "@/hooks/useUserActivity"
-import { SocketEvent } from "@/types/socket"
-import { USER_STATUS, User } from "@/types/user"
-import { useEffect } from "react"
-import Draggable from "react-draggable"
-import { useLocation, useNavigate, useParams } from "react-router-dom"
+import SplitterComponent from "@/components/SplitterComponent";
+import ConnectionStatusPage from "@/components/connection/ConnectionStatusPage";
+import Sidebar from "@/components/sidebar/Sidebar";
+import VideoCall from "@/components/sidebar/sidebar-views/VideoCall";
+import WorkSpace from "@/components/workspace";
+import { useAppContext } from "@/context/AppContext";
+import { useSocket } from "@/context/SocketContext";
+import useFullScreen from "@/hooks/useFullScreen";
+import useUserActivity from "@/hooks/useUserActivity";
+import { SocketEvent } from "@/types/socket";
+import { USER_STATUS } from "@/types/user";
+import { useEffect } from "react";
+import Draggable from "react-draggable";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 function EditorPage() {
-    // Listen user online/offline status
-    useUserActivity()
-    // Enable fullscreen mode
-    useFullScreen()
-    const navigate = useNavigate()
-    const { roomId } = useParams()
-    const { status, setCurrentUser, currentUser } = useAppContext()
-    const { socket } = useSocket()
-    const location = useLocation()
+    useUserActivity();
+    useFullScreen();
+
+    const navigate = useNavigate();
+    const { roomId } = useParams();
+    const { status, setCurrentUser, currentUser } = useAppContext();
+    const { socket } = useSocket();
+    const location = useLocation();
 
     useEffect(() => {
-        if (currentUser.username.length > 0) return
-        const username = location.state?.username
+        if (currentUser.username.length > 0) return;
+        const username = location.state?.username;
         if (username === undefined) {
             navigate("/", {
                 state: { roomId },
-            })
+            });
         } else if (roomId) {
-            const user: User = { username, roomId }
-            setCurrentUser(user)
-            socket.emit(SocketEvent.JOIN_REQUEST, user)
+            const user = { username, roomId };
+            setCurrentUser(user);
+            socket.emit(SocketEvent.JOIN_REQUEST, user);
         }
     }, [
         currentUser.username,
@@ -43,10 +42,10 @@ function EditorPage() {
         roomId,
         setCurrentUser,
         socket,
-    ])
+    ]);
 
     if (status === USER_STATUS.CONNECTION_FAILED) {
-        return <ConnectionStatusPage />
+        return <ConnectionStatusPage />;
     }
 
     return (
@@ -54,14 +53,14 @@ function EditorPage() {
             <Sidebar />
             <WorkSpace />
             <Draggable>
-                <div className=" absolute z-50 h-[200px] w-[200px] border-t border-darkHover bg-dark text-white">
+                <div className="absolute z-50 h-[200px] w-[200px] border-t border-darkHover bg-dark text-white">
                     <div className="flex h-full w-full flex-col items-center justify-center">
                         <VideoCall />
                     </div>
                 </div>
             </Draggable>
         </SplitterComponent>
-    )
+    );
 }
 
-export default EditorPage
+export default EditorPage;
