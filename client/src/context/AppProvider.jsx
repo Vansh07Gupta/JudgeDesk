8d880bbdbc05575524b3c6445150cadf7231384e
Vansh07Gupta <149710593+Vansh07Gupta@users.noreply.github.com>
2025-04-26 15:11:50 +0530

    Add Context API for client side

---
+++ client/src/context/AppProvider.jsx
@@ -0,0 +1,29 @@
+import { AppContextProvider } from "./AppContext.js";
+import { ChatContextProvider } from "./ChatContext.jsx";
+import { FileContextProvider } from "./FileContext.jsx";
+import { RunCodeContextProvider } from "./RunCodeContext.jsx";
+import { SettingContextProvider } from "./SettingContext.jsx";
+import { SocketProvider } from "./SocketContext.jsx";
+import { ViewContextProvider } from "./ViewContext.js";
+
+function AppProvider({ children }) {
+    return (
+        <AppContextProvider>
+            <SocketProvider>
+                <SettingContextProvider>
+                    <ViewContextProvider>
+                        <FileContextProvider>
+                            <RunCodeContextProvider>
+                                <ChatContextProvider>
+                                    {children}
+                                </ChatContextProvider>
+                            </RunCodeContextProvider>
+                        </FileContextProvider>
+                    </ViewContextProvider>
+                </SettingContextProvider>
+            </SocketProvider>
+        </AppContextProvider>
+    );
+}
+
+export default AppProvider;
